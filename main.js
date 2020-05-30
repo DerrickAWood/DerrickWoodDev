8e53b16ee566aed997d497f0c09299764da4bac5
Derrick A Wood <dwood_1994@yahoo.com>
2020-05-29 22:09:22 -0600

    push

---
+++ main.js
@@ -0,0 +1,22 @@
+
+function linkedIn(){
+  window.open("https://www.linkedin.com/in/derrickawood/")
+}
+function gitHub(){
+  window.open("https://github.com/DerrickAWood")
+}
+function email(){
+  // @ts-ignore
+  swal("dwood_1994@yahoo.com");
+}
+function cell(){
+  // @ts-ignore
+  swal("(661) 912-4085");
+}
+
+function localReach(){
+  window.open("https://localreach.herokuapp.com/#/")
+}
+function bugLogger(){
+  window.open("https://debug-logger.herokuapp.com/#/")
+}
